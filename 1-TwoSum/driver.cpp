ac815ac57b232faba85c482136b80ac380acbaa0
Jonathan Adalin <jonathanadalin@gmail.com>
2018-12-20 08:39:59 -0600

    Edited file structure

--- 1-TwoSum/driver.cpp
+++ 1-TwoSum/driver.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include "solution.h"
 
 int main() {
-  std::cout << "Hello, world." << std::endl;
+  if (two_sum::RunTests()) {
+    std::cout << "Success" << std::endl;
+  } else {
+    std::cout << "Hello, world." << std::endl;
+  }
 }
--- 1-TwoSum/solution.h
+++ 1-TwoSum/solution.h
@@ -1,4 +1,12 @@
-class TwoSum {
-public:
-  vector<int> twoSum(vector<int>& nums, int target);
+#include <vector>
+
+namespace two_sum {
+
+// Given an array of integers, return indices of the two numbers such that they
+// add up to a specific target. Assume one solution and no same element twice.
+std::vector<int> TwoSum(std::vector<int>& nums, int target);
+
+// Returns true if all the tests have passed.
+bool RunTests();
+	 
 } 
