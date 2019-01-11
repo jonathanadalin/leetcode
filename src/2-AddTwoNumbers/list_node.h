f04d314db4fa1d43f31d22ba8624c06de22a9265
Jonathan Adalin <jonathanadalin@gmail.com>
2019-01-11 15:16:22 -0600

    Added list node struct

---
+++ src/2-AddTwoNumbers/list_node.h
@@ -0,0 +1,10 @@
+#ifndef ADD_TWO_NUM_LIST_NODE_
+#define ADD_TWO_NUM_LIST_NODE_
+
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+#endif  // ADD_TWO_NUM_LIST_NODE_
