ea6d5b736f9d052ba06181f4872d14a664fdd502
Jonathan <jonathanadalin@gmail.com>
2019-02-24 19:42:39 -0600

    461: Minor modification and added runtime analysis.

--- src/461-HammingDistance/solution.cpp
+++ src/461-HammingDistance/solution.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // O(n) with n being the number of digits in x or y's binary representation.
     int hammingDistance(int x, int y) {
         int count = 0;
         int num_binary_digits;
@@ -7,9 +8,9 @@ public:
             return 0;
         }
         if (x > y) {
-            num_binary_digits = floor(log(x) / log(2))  + 1;
+            num_binary_digits = floor(log(x) / log(2)) + 1;
         } else {
-            num_binary_digits = floor(log(y) / log(2))  + 1;
+            num_binary_digits = floor(log(y) / log(2)) + 1;
         }
         int i = num_binary_digits - 1;
         int n;
