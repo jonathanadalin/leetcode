cd29560bcb142104bf739ec1864f0f6f17bbbfcf
Jonathan <jonathanadalin@gmail.com>
2019-02-24 19:32:12 -0600

    Added solution for 461-HammingDistance

---
+++ src/461-HammingDistance/solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int count = 0;
+        int num_binary_digits;
+        if (x == y) {
+            return 0;
+        }
+        if (x > y) {
+            num_binary_digits = floor(log(x) / log(2))  + 1;
+        } else {
+            num_binary_digits = floor(log(y) / log(2))  + 1;
+        }
+        int i = num_binary_digits - 1;
+        int n;
+        while (i >= 0) {
+            n = pow(2, i);
+            if (x - n >= 0 && y- n >= 0) {
+                x -= n;
+                y -= n;
+            } else if (x - n >= 0 && y - n < 0) {
+                count++;
+                x -= n;
+            } else if (y - n >= 0 && x - n < 0){
+                count++;
+                y -= n;
+            }
+            i--;
+        }
+        return count;
+    }
+};
