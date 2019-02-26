347aea31c3c19b02b4d5cad8ecb8b0928646d3f9
Jonathan <jonathanadalin@gmail.com>
2019-02-25 23:07:42 -0600

    Added 617 solution,

---
+++ src/617-MergeTwoBinaryTrees/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if (t1 != NULL && t2 != NULL) {
+            TreeNode* t = new TreeNode(t1->val + t2->val);
+            t->left = mergeTrees(t1->left, t2->left);
+            t->right = mergeTrees(t1->right, t2->right);
+            return t;
+        } else if (t1 != NULL) {
+            return t1;
+        } else if (t2 != NULL) {
+            return t2;
+        } else
+            return NULL;
+    }
+};
