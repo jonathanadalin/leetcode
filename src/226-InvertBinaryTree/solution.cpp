08edd62a271e749429a5a8923d76ea8201fd3db6
Jonathan <jonathanadalin@gmail.com>
2019-02-26 22:18:45 -0600

    Added solution for inverting a binary tree

---
+++ src/226-InvertBinaryTree/solution.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == NULL) {
+            return NULL;
+        } else if (root->left != NULL && root->right != NULL) {
+                TreeNode* temp = root->left;
+                root->left = invertTree(root->right);
+                root->right = invertTree(temp);
+            
+        } else if (root->left != NULL) {
+            root->right = invertTree(root->left);
+            root->left = NULL;
+        } else if (root->right != NULL) {
+            root->left = invertTree(root->right);
+            root->right = NULL;
+        }
+        return root;  // Don't need to do anything.
+    }
+};
