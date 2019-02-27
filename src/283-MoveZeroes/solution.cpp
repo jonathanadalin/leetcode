0aa4d052f4f0197536e6efd9828ab4387098854b
Jonathan <jonathanadalin@gmail.com>
2019-02-26 22:38:47 -0600

    Added solution for 283

---
+++ src/283-MoveZeroes/solution.cpp
@@ -0,0 +1,22 @@
+// Given an array nums, write a function to move all 0's to the end of it while
+// maintaining the relative order of the non-zero elements.
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int non_zero_index = 0;
+        int all_elem_index = 0;
+        while (all_elem_index < nums.size()) {
+            if (nums[all_elem_index] != 0) {
+                nums[non_zero_index] = nums[all_elem_index];
+                non_zero_index++;  // Only iterate if non-zero value.
+            }
+            all_elem_index++;
+        }
+        // The rest of the values are 0s.
+        while (non_zero_index < nums.size()) {
+            nums[non_zero_index] = 0;
+            non_zero_index++;
+        }
+    }
+};
