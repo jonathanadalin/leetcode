900fd2ec7fbff1b85cfab1628535a2487c1bd37f
Jonathan <jonathanadalin@gmail.com>
2019-02-28 20:12:27 -0600

    448: Added new solution, which is slightly faster

---
+++ src/448-FindAllNumbersDisappearedInAnArray/solution.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int j;
+        for (int i = 0; i < nums.size(); i++) {
+            j = abs(nums[i]) - 1;
+            if (nums[j] > 0) {
+                nums[j] = -nums[j];
+            }
+        }
+        vector<int> v;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) {
+                v.push_back(i + 1);
+            }
+        }
+        return v;
+    }
+
+    // O(n)
+    vector<int> findDisappearedNumbersOld(vector<int>& nums) {
+
+        // O(n)
+        std::set<int> all_nums;
+        for (int i = 1; i <= nums.size(); i++) {
+            all_nums.insert(i);
+        }
+
+        // O(n)
+        std::set<int> our_nums;
+        for (int i = 0; i < nums.size(); i++) {
+            our_nums.insert(nums[i]);
+        }
+        std::set<int> diff;
+        
+        // O(n)
+        std::set_difference(all_nums.begin(), all_nums.end(),
+                            our_nums.begin(), our_nums.end(),
+                            std::inserter(diff, diff.end()));
+        // O(n)
+        std::vector<int> v;
+        for (std::set<int>::iterator it = diff.begin(); it != diff.end(); ++it) {
+            v.push_back(*it);
+        }
+        return v;
+    }
+};
