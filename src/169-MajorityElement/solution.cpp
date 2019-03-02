c7076f12d22ecfdae926af66ebecae24b26a7262
Jonathan <jonathanadalin@gmail.com>
2019-03-01 18:12:07 -0600

    169: Added solution

---
+++ src/169-MajorityElement/solution.cpp
@@ -0,0 +1,15 @@
+#include <map>
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        std::map<int, int> m;
+        for (int i = 0; i < nums.size(); i++) {
+            m[nums[i]]++;
+            if (m[nums[i]] > nums.size()/2) {
+                return nums[i];
+            }
+        }
+        return 0;
+    }
+};
