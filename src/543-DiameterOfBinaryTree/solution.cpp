6f2bc9139c75eaffdd5db757bb4fffbf42e87eab
Jonathan <jonathanadalin@gmail.com>
2019-03-02 11:45:51 -0600

    543: Reduced the stack call by storing tree height

--- src/543-DiameterOfBinaryTree/solution.cpp
+++ src/543-DiameterOfBinaryTree/solution.cpp
@@ -11,29 +11,29 @@
 class Solution {
 public:
     
-    int getBranchHeight(TreeNode* node) {
+    int getHeights(TreeNode* node) {
         if (node == NULL) {
             return 0;
         } else {
-            return 1 + std::max(getBranchHeight(node->left),
-                                getBranchHeight(node->right));
+            node->val = 1 + std::max(getHeights(node->left),
+                                     getHeights(node->right));
         }
+        return node->val;
     }
     
     int diameterOfBinaryTree(TreeNode* root) {
         if (root == NULL) {
             return 0;
         }
+        getHeights(root);
         if (root->left != NULL && root->right != NULL) {
-            return std::max(getBranchHeight(root->left) + getBranchHeight(root->right),
+            return std::max(root->left->val + root->right->val,
                             std::max(diameterOfBinaryTree(root->left),
                                      diameterOfBinaryTree(root->right)));
         } else if (root->left != NULL) {
-            return std::max(diameterOfBinaryTree(root->left),
-                            getBranchHeight(root->left));
+            return std::max(diameterOfBinaryTree(root->left), root->left->val);
         } else if (root->right != NULL) {
-            return std::max(diameterOfBinaryTree(root->right),
-                            getBranchHeight(root->right));
+            return std::max(diameterOfBinaryTree(root->right), root->right->val);
         } else {  // No children
             return 0; 
         }
