bbd53a31d4880130d28804275b77f58f23306712
Jonathan <jonathanadalin@gmail.com>
2019-03-02 18:54:00 -0600

    121: Added solution

---
+++ src/121-BestTimeToBuyAndSellStock/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {     
+        if (prices.size() < 2) {
+            return 0;
+        }
+        int dp[prices.size()];
+        dp[0] = 0;  // No transcations
+        dp[1] = std::max(dp[0], prices[1] - prices[0]);
+        int min_buy_price = std::min(prices[0], prices[1]);
+        for (int i = 2; i < prices.size(); i++) {
+            dp[i] = std::max(dp[i - 1], prices[i] - min_buy_price);
+            min_buy_price = std::min(prices[i], min_buy_price);
+        }
+        return dp[prices.size() - 1];
+    }
+};
