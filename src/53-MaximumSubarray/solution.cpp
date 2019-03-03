90bd2a8c67ce668b47afe4601e30ca83d4738819
Jonathan <jonathanadalin@gmail.com>
2019-03-03 16:49:55 -0600

    53: Added DP solution O(n)

--- src/53-MaximumSubarray/solution.cpp
+++ src/53-MaximumSubarray/solution.cpp
@@ -1,7 +1,24 @@
 class Solution {
 public:
+    // Dynamic Programming, O(n)
+    int maxSubArrayDP(vector<int>& nums) {
+        if (nums.size() == 0) {
+            return 0;
+        }
+        int dp[nums.size()];
+        dp[0] = nums[0];
+        int cont_sum = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            cont_sum += nums[i];
+            if (cont_sum < nums[i]) {  // Should start a new continuous sum.
+                cont_sum = nums[i];
+            }
+            dp[i] = std::max(dp[i - 1], cont_sum);
+        }
+        return dp[nums.size() - 1];
+    }
     // Brute Force, O(n^2)
-    int maxSubArray(vector<int>& nums) {
+    int maxSubArrayBF(vector<int>& nums) {
         if (nums.size() == 0) {
             return 0;
         }
