0cc465907e9159723dc6b7d14d96435de850974f
Jonathan <jonathanadalin@gmail.com>
2019-03-17 14:14:04 -0500

    438: Modified to account for duplicates in p

--- src/438-FindAllAnagrams/solution.cpp
+++ src/438-FindAllAnagrams/solution.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     
-    void populateSet(unordered_set<char> &set, string p) {
+    void populateSet(map<char, int> &m, const string p) {
        for (int i = 0; i < p.length(); i++) {
-           set.insert(p[i]);
+           if (m.find(p[i]) != m.end()) {
+                m[p[i]]++;
+           } else {
+                m.insert(std::pair<char, int>(p[i],0));
+           }
        } 
     }
     
@@ -12,16 +16,22 @@ public:
         if (s.length() < p.length()) {
             return v;
         }
-        unordered_set<char> set;
+        map<char, int> m;
         for (int i = 0; i < s.length() - p.length() + 1; i++) {
-            populateSet(set, p);
+            populateSet(m, p);
             for (int j = i; j < p.length() + i; j++) {
-                set.erase(s[j]);
+                if (m.find(s[j]) != m.end()) {
+                    if (m[s[j]] > 0) {
+                        m[s[j]]--;  
+                    } else {
+                        m.erase(s[j]);
+                    }        
+                }
             }
-            if (set.size() == 0) {
+            if (m.size() == 0) {
                 v.push_back(i);
             }
-            set.clear();
+            m.clear();
         }
         return v;
     }
