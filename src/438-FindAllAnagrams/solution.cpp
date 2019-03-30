367e61e248f830ab1e76d3c8cb8420559cdf8ba9
Jonathan <jonathanadalin@gmail.com>
2019-03-30 18:15:07 -0500

    438: Removed expensive populate functions

--- src/438-FindAllAnagrams/solution.cpp
+++ src/438-FindAllAnagrams/solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    void populateSet(map<char, int> &m, const string p) {
+    void populateMap(map<char, int> &m, const string p) {
        for (int i = 0; i < p.length(); i++) {
            if (m.find(p[i]) != m.end()) {
                 m[p[i]]++;
@@ -11,27 +11,35 @@ public:
        } 
     }
     
+    void copyMap(map<char, int> &populated_map, map<char, int> &working_map) {
+        for (map<char, int>::iterator it = populated_map.begin(); it != populated_map.end(); it++) {
+            working_map.insert(pair<char, int>((*it).first,(*it).second));
+        }
+    }
+    
     vector<int> findAnagrams(string s, string p) {
         vector<int> v;
         if (s.length() < p.length()) {
             return v;
         }
-        map<char, int> m;
+        map<char, int> populated_map;
+        populateMap(populated_map, p);
+        map<char, int> working_map;
         for (int i = 0; i < s.length() - p.length() + 1; i++) {
-            populateSet(m, p);
+            copyMap(populated_map, working_map);
             for (int j = i; j < p.length() + i; j++) {
-                if (m.find(s[j]) != m.end()) {
-                    if (m[s[j]] > 0) {
-                        m[s[j]]--;  
+                if (working_map.find(s[j]) != working_map.end()) {
+                    if (working_map[s[j]] > 0) {
+                        working_map[s[j]]--;  
                     } else {
-                        m.erase(s[j]);
+                        working_map.erase(s[j]);
                     }        
                 }
             }
-            if (m.size() == 0) {
+            if (working_map.size() == 0) {
                 v.push_back(i);
             }
-            m.clear();
+            working_map.clear();
         }
         return v;
     }
