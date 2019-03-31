1457125939806bae13fdcaf1db26e77615e0d0c5
Jonathan <jonathanadalin@gmail.com>
2019-03-31 14:38:56 -0500

    438: Added 'window' optimization, but a few things left to debug

--- src/438-FindAllAnagrams/solution.cpp
+++ src/438-FindAllAnagrams/solution.cpp
@@ -25,20 +25,43 @@ public:
         map<char, int> populated_map;
         populateMap(populated_map, p);
         map<char, int> working_map;
+        bool last_set_pass = false;
+        bool skip_expensive_op = false;
         for (int i = 0; i < s.length() - p.length() + 1; i++) {
-            copyMap(populated_map, working_map);
-            for (int j = i; j < p.length() + i; j++) {
-                if (working_map.find(s[j]) != working_map.end()) {
-                    if (working_map[s[j]] > 0) {
-                        working_map[s[j]]--;  
-                    } else {
-                        working_map.erase(s[j]);
-                    }        
+            
+            // We don't need to check all the whole string if the last window passed.
+            // We're good if the newest element is the same as the one we stopped looking at.
+            if (last_set_pass) {
+                skip_expensive_op = true;
+                if (s[p.length() + i - 1] == s[i - 1]) {
+                    v.push_back(i);
+                } else {
+                    last_set_pass = false;
                 }
             }
-            if (working_map.size() == 0) {
-                v.push_back(i);
+            
+            if (!skip_expensive_op) {
+                // O(p.length()), which is expensive if <p> is long.
+                copyMap(populated_map, working_map);
+                for (int j = i; j < p.length() + i; j++) {    
+                    if (working_map.find(s[j]) != working_map.end()) {
+                        if (working_map[s[j]] > 0) {
+                            working_map[s[j]]--;  
+                        } else {
+                            working_map.erase(s[j]);
+                        }        
+                    }
+                }
+
+                if (working_map.size() == 0) {
+                    v.push_back(i);
+                    last_set_pass = true;
+                } else {
+                    last_set_pass = false;
+                    skip_expensive_op = false;
+                }
             }
+
             working_map.clear();
         }
         return v;
