a99fa8c24deaa17c26a62a53fd2dbaf01c955c2a
Jonathan <jonathanadalin@gmail.com>
2019-04-01 16:55:16 -0500

    155: Optimized getMin(), top 15% solution

--- src/155-MinStack/solution.cpp
+++ src/155-MinStack/solution.cpp
@@ -3,32 +3,50 @@ public:
     /** initialize your data structure here. */
     
     std::vector<int> v;
+    int min;
+    bool min_known;
     
     MinStack() {
-
+        min_known = false;
     }
     
+    // O(1)
     void push(int x) {
+        if (v.size() == 0) {
+            min = x;
+        } else {
+            if (x < min) {
+                min = x;
+            }
+        }
         v.push_back(x);
     }
     
+    // O(1)
     void pop() {
+        if (min == v[v.size() - 1]) {
+            min_known = false;
+        }
         v.erase(v.begin() + v.size() - 1);
     }
     
+    // O(1)
     int top() {
         return v[v.size() - 1];
     }
     
+    // O(1) usually, worst case O(n)
     int getMin() {
-        if (v.size() == 0) {
-            return -1;
-        }
-        int min = v[0];
-        for (int i = 1; i < v.size(); i++) {
-            if (v[i] < min) {
-                min = v[i];
+        if (!min_known) {
+            if (v.size() > 0) {
+                min = v[0];
             }
+            for (int i = 1; i < v.size(); i++) {
+                if (v[i] < min) {
+                    min = v[i];
+                }
+            }
+            min_known = true;
         }
         return min;
     }
