f55611cbd0f3d904a2d83c103adc0d75f2c18b0e
Jonathan <jonathanadalin@gmail.com>
2019-04-06 17:15:28 -0500

    160: Added runtime analysis

--- src/160-IntersectionOfTwoLinkedLists/solution.java
+++ src/160-IntersectionOfTwoLinkedLists/solution.java
@@ -13,12 +13,15 @@
 import java.util.*;
 
 public class Solution {
+    
+    // O(n)
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         
         if (headA == null || headB == null) {
             return null;
         }
         
+        // O(n)
         Vector<ListNode> vA = new Vector<ListNode>();
         ListNode nodeA = headA;
         while (nodeA != null) {
@@ -26,6 +29,7 @@ public class Solution {
             nodeA = nodeA.next;
         }
         
+        // O(n)
         Vector<ListNode> vB = new Vector<ListNode>();
         ListNode nodeB = headB;
         while (nodeB != null) {
@@ -40,6 +44,7 @@ public class Solution {
         int iA = vA.size() - 2;
         int iB = vB.size() - 2;
         
+        // O(n) at worst
         while (iA >= 0 && iB >= 0) {
             if (vA.get(iA) == vB.get(iB)) {
                 iA--;
@@ -49,6 +54,7 @@ public class Solution {
             }
         }
         
+        // O(1) Access time is constant
         return vA.get(iA + 1);
         
     }
