6b04874516fa33a9b8836455f104d5d31f8f5066
Jonathan <jonathanadalin@gmail.com>
2019-04-10 16:08:05 -0500

    237: Added runtime analysis

--- src/237-DeleteNodeInALinkedList/solution.cpp
+++ src/237-DeleteNodeInALinkedList/solution.cpp
@@ -11,6 +11,7 @@ class Solution {
 public:
     // We don't have access to the head, so just go through all the nodes
     // after the one we have to delete and copy their values.
+    // O(n) at worst, we go through the whole list at worst.
     void deleteNode(ListNode* node) {
         while (node->next != NULL) {  // There is at least 2 nodes.
             node->val = node->next->val;
