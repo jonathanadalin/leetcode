7c6775ba91777b6553f039b75ab540082e01234f
Jonathan <jonathanadalin@gmail.com>
2019-04-10 16:07:00 -0500

    237: Added solution.

---
+++ src/237-DeleteNodeInALinkedList/solution.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    // We don't have access to the head, so just go through all the nodes
+    // after the one we have to delete and copy their values.
+    void deleteNode(ListNode* node) {
+        while (node->next != NULL) {  // There is at least 2 nodes.
+            node->val = node->next->val;
+            if (node->next->next == NULL) {  // The next node is at the end.
+                node->next = NULL;
+            } else {
+                node = node->next;
+            }
+        }
+        node = NULL;
+    }
+};
