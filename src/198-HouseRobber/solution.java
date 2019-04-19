0189a7b10af01baaa4ae0caeacac0b37cbf1d038
Jonathan <jonathanadalin@gmail.com>
2019-04-18 23:35:24 -0500

    198: Added Java runtime analysis and comments

--- src/198-HouseRobber/solution.java
+++ src/198-HouseRobber/solution.java
@@ -1,4 +1,5 @@
 class Solution {
+    // O(n)
     public int rob(int[] nums) {
         if (nums.length < 1) {
             return 0;
@@ -8,8 +9,10 @@ class Solution {
         int dp[] = new int[nums.length];
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
+        // O(n)
         for (int i = 2; i < nums.length; ++i) {
-            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+            dp[i] = Math.max(dp[i - 1],             // Don't rob the current house.
+                             dp[i - 2] + nums[i]);  // Rob the current house.
         }
         return dp[nums.length - 1];
     }
