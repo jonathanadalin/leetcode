a66e1ab7bf156289374589dad40e6ef730ffe4f7
Jonathan <jonathanadalin@gmail.com>
2019-04-21 21:11:11 -0500

    122: Added explanation

--- src/122-BestTimeToBuyAndSellStockII/solution.java
+++ src/122-BestTimeToBuyAndSellStockII/solution.java
@@ -1,5 +1,16 @@
 class Solution {
     // O(n) - We iterate through prices once
+    //
+    // The net sum of all the profits is equal to
+    // to the optimal purchase and sell order gain.
+    // 
+    // You can compare this to calculating the distance
+    // between two points with intermediate points
+    // 
+    // eg.
+    // a .. b .. c .. d
+    // The distance a to d, |ad|, is the same as
+    // |ab| + |bc| + |cd|
     public int maxProfit(int[] prices) {
         if (prices.length < 2) {
             return 0;
