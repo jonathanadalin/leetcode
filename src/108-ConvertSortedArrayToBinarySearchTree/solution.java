2b75a50f4af66ae6062251677269c87349f1c000
Jonathan <jonathanadalin@gmail.com>
2019-04-22 19:51:41 -0500

    108: Added iterative solution

--- src/108-ConvertSortedArrayToBinarySearchTree/solution.java
+++ src/108-ConvertSortedArrayToBinarySearchTree/solution.java
@@ -12,8 +12,56 @@ class Solution {
     
     // O(n) - We need to place all the values of nums
     //        into the BST.
+
+    public TreeNode sortedArrayToBSTIterative(int[] nums) {
+        
+        if (nums == null) {
+            return null;
+        }
+        
+        if (nums.length == 0) {
+            return null;
+        }
+        
+        Stack<Object> s = new Stack<Object>();
+        // We're going to overwrite this value later.
+        TreeNode root = new TreeNode(0);
+        s.push(root);
+        s.push(0);
+        s.push(nums.length);
+        
+        while (!s.empty()) {
+            
+            int right = (int) s.pop();
+            int left = (int) s.pop();
+            TreeNode n = (TreeNode) s.pop();
+            
+            int mid = (left + right) / 2;
+            if (mid >= 0 && mid < nums.length) {
+                n.val = nums[mid];
+            }
+            
+            if (left < mid) {
+                n.left = new TreeNode(0);
+                s.push(n.left);
+                s.push(left);
+                s.push(mid);
+            }
+            
+            if (right > mid + 1) {
+                n.right = new TreeNode(0);
+                s.push(n.right);
+                s.push(mid + 1);
+                s.push(right);
+            }
+            
+        }
+        
+        return root;
+
+    }
     
-    public TreeNode sortedArrayToBST(int[] nums) {
+    public TreeNode sortedArrayToBSTRecursive(int[] nums) {
         if (nums.length == 0) {
             return null;
         }
