d952a0cd42fc263de59c1961dfde0dc5afaf0dce
Jonathan <jonathanadalin@gmail.com>
2019-04-23 19:11:31 -0500

    387: Added two solutions

---
+++ src/387-FirstUniqueCharacterInAString/solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    
+    public int firstUniqChar(String s) {
+        int[] freq = new int[26];  // 26 letters in alphabet.
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] == 1) {
+                return i;   
+            }
+        }
+        return -1;  //  All letters have duplicates.
+    }
+    
+    
+    public int firstUniqCharHashMap(String s) {
+        HashMap<Character, Integer> hm = new HashMap();
+        for (int i = 0; i < s.length(); i++) {
+            if (hm.containsKey(s.charAt(i))) {
+                hm.replace(s.charAt(i), hm.get(s.charAt(i)) + 1);
+            } else {
+                hm.put(s.charAt(i), 0);
+            }
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (hm.get(s.charAt(i)) == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+}
