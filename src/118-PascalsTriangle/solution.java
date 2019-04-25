d231eb62842f22fec8edf58b08f1c1ac7c72188e
Jonathan <jonathanadalin@gmail.com>
2019-04-24 22:17:05 -0500

    118: Added runtime analysis

--- src/118-PascalsTriangle/solution.java
+++ src/118-PascalsTriangle/solution.java
@@ -1,4 +1,5 @@
 class Solution {
+    // O(n + n-1 + n-2 + ... + 1) = O((n^2 + n) / 2)
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> triangle = new ArrayList();
         for (int row = 0; row < numRows; row++) {
