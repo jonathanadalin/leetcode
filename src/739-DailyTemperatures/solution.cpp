1199e6a5913bc0769e00614f495730348a1ade59
Jonathan <jonathanadalin@gmail.com>
2019-05-03 19:10:14 -0500

    739: Added O(n) solution

--- src/739-DailyTemperatures/solution.cpp
+++ src/739-DailyTemperatures/solution.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
+
+    // O(n) - Single pass of T
     vector<int> dailyTemperatures(vector<int>& T) {
+        vector<int> days_to_wait(T.size(), 0);
+        if (T.size() == 0) {
+            return days_to_wait;
+        }
+        stack<int> indexes;
+        for (int i = 0; i < T.size(); i++) {
+            while(!indexes.empty() && T[i] > T[indexes.top()]) {
+                int index = indexes.top();
+                days_to_wait[index] = i - index;
+                indexes.pop();
+            }
+            indexes.push(i);
+        }
+        return days_to_wait;
+    }
+
+    // O(n^2) - Nested for loop of T
+    vector<int> dailyTemperatures_slow(vector<int>& T) {
         vector<int> v;
         int days_to_wait;
         for (int i = 0; i < T.size(); i++) {
@@ -19,4 +39,5 @@ public:
         }
         return v;
     }
+
 };
