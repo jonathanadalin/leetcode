a7432551dbdeda20cdb671d307268352379fd730
Jonathan Adalin <jonathanadalin@gmail.com>
2019-05-05 18:20:36 -0400

    647: Added optimal O(n^2) solution with runtime analysis

--- src/647-PalindromicSubstrings/solution.cpp
+++ src/647-PalindromicSubstrings/solution.cpp
@@ -1,8 +1,33 @@
 class Solution {
 public:
 
+    int count = 0;
+
+    // O(n) - Worst case, entire string extended is a palindrome
+    void extendPalindrome(const string &s, int left, int right) {
+        while(left >= 0 && right < s.length() && s.at(left) == s.at(right)) {
+            count++;
+            left--;
+            right++;
+        }
+    }  
+
+    // O(n^2) - For loop with 2 * O(n) function calls
+    int countSubstrings(string s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            extendPalindrome(s, i, i);      // Odd palindromes
+            extendPalindrome(s, i, i + 1);  // Even palindromes
+        }
+        return count;
+    }
+
+    // Below is older solution...
+
     // O(n) - Worst case, go through <s>
-    bool isPalindrome(const string &s, int left, int right) {
+    bool isPalindrome_old(const string &s, int left, int right) {
         while (left < right) {
             if (s.at(left) == s.at(right)) {
                 left++;
@@ -26,5 +51,5 @@ public:
         }
         return count;
     }
-    
+
 };
