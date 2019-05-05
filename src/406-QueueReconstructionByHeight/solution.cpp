4fcd5ec54b79865d4b0641cc1fd93b9bdc33d79a
Jonathan Adalin <jonathanadalin@gmail.com>
2019-05-05 17:28:41 -0400

    406: Added solution

---
+++ src/406-QueueReconstructionByHeight/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        auto comp = [](const vector<int> &v1, const vector<int> &v2) {
+            return (v1[0] > v2[0] || (v1[0] == v2[0] && v1[1] < v2[1]));                 
+        };
+        sort(people.begin(), people.end(), comp);        
+        vector<vector<int>> res;
+        for (auto& p : people) 
+            res.insert(res.begin() + p[1], p);
+        return res;
+    }
+};
