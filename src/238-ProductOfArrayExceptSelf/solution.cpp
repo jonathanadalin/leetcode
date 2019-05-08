9e1ee378ba03aad34160c2adc49a502142a0d69f
Jonathan Adalin <jonathanadalin@gmail.com>
2019-05-07 21:13:59 -0400

    238: Added O(n^2) solution, but can improve

--- src/238-ProductOfArrayExceptSelf/solution.cpp
+++ src/238-ProductOfArrayExceptSelf/solution.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int product = 1;
-        for (int num : nums) {
-            product *= num;
-        }
         vector<int> v;
-        for (auto it = nums.begin(); it != nums.end(); ++it) {
-            v.push_back(product / *it);
+        for (int i = 0; i < nums.size(); i++) {
+            int product = 1;
+            for (int j = 0; j < nums.size(); j++) {
+                if (j != i) {
+                    product *= nums[j];
+                }
+            }
+            v.push_back(product);
         }
         return v;
     }
