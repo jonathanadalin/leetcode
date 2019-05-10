48aa07f85e9483be12c545b6c6f3820858c692f0
Jonathan Adalin <jonathanadalin@gmail.com>
2019-05-09 21:52:17 -0400

    78: Added solution

---
+++ src/78-Subsets/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    
+    void subsets_recursive(const vector<int> &nums, int current,
+                           vector<int> v, vector<vector<int>> &res) {
+        for (int i = current; i < nums.size(); i++) {
+            v.push_back(nums[i]);
+            res.push_back(v);
+            subsets_recursive(nums, i + 1, v, res);
+            v.erase(v.end() - 1);
+        }
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> v;
+        res.push_back(v);
+        subsets_recursive(nums, 0, v, res);
+        return res;
+    }
+};
