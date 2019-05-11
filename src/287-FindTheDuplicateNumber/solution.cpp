1941f4f440108b261d2e807723a11f457cc0db40
Jonathan Adalin <jonathanadalin@gmail.com>
2019-05-11 15:21:33 -0400

    287: Added new solution that uses O(1) space. Explanation inline

--- src/287-FindTheDuplicateNumber/solution.cpp
+++ src/287-FindTheDuplicateNumber/solution.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
+
+    // O(n)
     int findDuplicate(vector<int>& nums) {
+        if (nums.size() > 0) {
+            int slow = nums[0];
+            int fast = nums[nums[0]];
+            // Since we are iterating using values as indexes,
+            // they will cross within the cycle that is created
+            // by the duplicate value.
+            while (fast != slow) {
+                slow = nums[slow];
+                fast = nums[nums[fast]];
+            }
+            // Fast and slow have met in the circle.
+            // If we reset fast, and go a slow's pace
+            // They'll meet at the circle entrance, which
+            // is the duplicate value.
+            fast = 0;
+            while (fast != slow) {
+                slow = nums[slow];
+                fast = nums[fast];
+            }
+            return fast;
+        }
+        return -1;
+    }
+
+    // O(n)
+    int findDuplicateWithSet(vector<int>& nums) {
         unordered_set<int> s;
         for (int num : nums) {
             if (s.find(num) == s.end()) {
