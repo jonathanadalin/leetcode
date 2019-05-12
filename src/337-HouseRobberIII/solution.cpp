d153cab2232a6df4cee6f9f4387cad3162ff3349
Jonathan <jonathanadalin@gmail.com>
2019-05-12 13:59:39 -0500

    337: Added DP solution

--- src/337-HouseRobberIII/solution.cpp
+++ src/337-HouseRobberIII/solution.cpp
@@ -11,6 +11,35 @@
 class Solution {
 public:
     
+    int rob_dp(TreeNode* root, map<TreeNode*, int>& m) {
+        if (root == NULL) {
+            return NULL;
+        }
+        // Don't recurse if we already found the value before.
+        auto it = m.find(root);
+        if (it != m.end()) {
+            return (*it).second;
+        }
+        int lower_level_val = 0;
+        if (root->left != NULL) {
+            lower_level_val += rob_dp(root->left->left, m) + rob_dp(root->left->right, m);
+        }
+        if (root->right != NULL) {
+            lower_level_val += rob_dp(root->right->left, m) + rob_dp(root->right->right, m);
+        }
+        // Choose between taking this level or the next.
+        int val = max(root->val + lower_level_val, rob_dp(root->left, m) + rob_dp(root->right, m));
+        m.insert(make_pair(root, val));
+        return val;
+    }
+    
+    int rob_dp(TreeNode* root) {
+        map<TreeNode*, int> m;
+        return rob_dp(root, m);
+    }
+
+    // Less optimal solution below...
+
     int rob(TreeNode* root, bool just_robbed) {
         if (root == NULL) {
             return 0;
