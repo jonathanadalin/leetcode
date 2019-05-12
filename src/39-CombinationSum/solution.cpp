cc287feeca0b04d6b3f77a187cf736c879f3053e
Jonathan <jonathanadalin@gmail.com>
2019-05-12 11:14:41 -0500

    39: Passed working vector by reference, reducing runtime by 3x -> top percentile solution

--- src/39-CombinationSum/solution.cpp
+++ src/39-CombinationSum/solution.cpp
@@ -2,17 +2,19 @@ class Solution {
 public:
         
     void populateRes(const vector<int>& candidates, int index,
-                     vector<int> v, int target, vector<vector<int>>& res) {
+                     vector<int> &v, int target, vector<vector<int>>& res) {
         if (index < candidates.size() && candidates[index] <= target) {
             if (candidates[index] == target) {
                 v.push_back(candidates[index]);
                 res.push_back(v);
+                v.pop_back();
             } else {
-                // Go to the next value.
-                populateRes(candidates, index + 1, v, target, res);  
-                v.push_back(candidates[index]);
+                // Branch off with the next candidate.
+                populateRes(candidates, index + 1, v, target, res);
                 // We can use repeated numbers.
+                v.push_back(candidates[index]);
                 populateRes(candidates, index, v, target - candidates[index], res);
+                v.pop_back();
             }
         }
     }
