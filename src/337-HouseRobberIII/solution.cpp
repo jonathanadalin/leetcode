d0e0388ffd589e61a55a9dd47bae1609ae8affa5
Jonathan <jonathanadalin@gmail.com>
2019-05-12 14:02:23 -0500

    337: Added runtime analysis, minor formatting

--- src/337-HouseRobberIII/solution.cpp
+++ src/337-HouseRobberIII/solution.cpp
@@ -10,7 +10,8 @@
 
 class Solution {
 public:
-    
+    // O(size of tree) - We only calculate each node's value once.
+    // Intermediate operations are constant amortized time O(1).
     int rob_dp(TreeNode* root, map<TreeNode*, int>& m) {
         if (root == NULL) {
             return NULL;
@@ -22,38 +23,21 @@ public:
         }
         int lower_level_val = 0;
         if (root->left != NULL) {
-            lower_level_val += rob_dp(root->left->left, m) + rob_dp(root->left->right, m);
+            lower_level_val += rob_dp(root->left->left, m) 
+                             + rob_dp(root->left->right, m);
         }
         if (root->right != NULL) {
-            lower_level_val += rob_dp(root->right->left, m) + rob_dp(root->right->right, m);
+            lower_level_val += rob_dp(root->right->left, m) 
+                             + rob_dp(root->right->right, m);
         }
         // Choose between taking this level or the next.
-        int val = max(root->val + lower_level_val, rob_dp(root->left, m) + rob_dp(root->right, m));
+        int val = max(root->val + lower_level_val,
+                      rob_dp(root->left, m) + rob_dp(root->right, m));
         m.insert(make_pair(root, val));
         return val;
-    }
-    
-    int rob_dp(TreeNode* root) {
+    }  
+    int rob(TreeNode* root) {
         map<TreeNode*, int> m;
         return rob_dp(root, m);
-    }
-
-    // Less optimal solution below...
-
-    int rob(TreeNode* root, bool just_robbed) {
-        if (root == NULL) {
-            return 0;
-        }
-        if (just_robbed) {
-            return (rob(root->left, false) + rob(root->right, false));               // Must skip over this house
-        } else {
-            // You have a choice...
-            return (max(root->val + rob(root->left, true) + rob(root->right, true),  // Rob current house
-            rob(root->left, false) + rob(root->right, false)));                      // Rob next house
-        }
-    }
-    
-    int rob(TreeNode* root) {
-        return rob(root, false);
     }
 };
