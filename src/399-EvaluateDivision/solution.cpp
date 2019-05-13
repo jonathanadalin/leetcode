7b2692191869ed76ea9e8a017015702bcd244d02
Jonathan <jonathanadalin@gmail.com>
2019-05-12 20:03:44 -0500

    399: Improved explanation

--- src/399-EvaluateDivision/solution.cpp
+++ src/399-EvaluateDivision/solution.cpp
@@ -12,6 +12,9 @@ public:
             will be represented with two paths:
             A -> k   -> B
             B -> 1/k -> A
+            
+        We can obtain the desired outcome by traversing the populating graph
+        and multiplying the weights if the connection isn't direct.
      */
     
     vector<double> calcEquation(vector<vector<string>>& equations,
@@ -55,13 +58,14 @@ public:
     
 private:
     
-    unordered_set<string> visited;
+    unordered_set<string> visited;  // Avoid stack overflow.
     double bfs(const string &start, const string &finish,
                const unordered_map<string,
                                    vector<pair<string, double>>> &nodes) {
         if (visited.find(start) == visited.end()) {
-            visited.insert(start);  // Don't check this start point again.
+            visited.insert(start);  
             // We know it exists because of previous checks.
+            // Check if it's a direct connection.
             auto node_it = nodes.find(start);
             for (auto weighted_path_pair : (*node_it).second) {
                 if (finish == weighted_path_pair.first) {
