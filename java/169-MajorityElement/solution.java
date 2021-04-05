336b9bbffe53e1bccfc461815a669de58f7ed030
Jonathan <jonathanadalin@gmail.com>
2021-04-05 00:10:55 -0700

    Add slightly better solution for 169-MajorityElement

--- java/169-MajorityElement/solution.java
+++ java/169-MajorityElement/solution.java
@@ -1,4 +1,13 @@
+/**
+ * Given an array nums of size n, return the majority element.
+ *
+ * The majority element is the element that appears more than ⌊n / 2⌋ times. You
+ * may assume that the majority element always exists in the array.
+ */
+
 class Solution {
+
+    // log(N)
     public int majorityElement(int[] nums) {
         var countMap = new HashMap<Integer, Integer>();
         for (var num : nums) {
@@ -16,5 +25,23 @@ class Solution {
             }
         }
         return majorityElement;
+    }
+
+    /**
+     * Boyer-Moore Voting Algorithm
+     * It is impossible to discard subsections where there are more majority
+     * elements than minority elements when count equals 0. We can discard
+     * that subsection and solve for the remaining recursively.
+     */
+    public int majorityElementBoyerMoore(int[] nums) {
+        var candidate = nums[0]; // Assume at least size 1 list.
+        var count = 0;
+        for (var num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count = (num == candidate)? count + 1 : count - 1;
+        }
+        return candidate;
     }
 }
