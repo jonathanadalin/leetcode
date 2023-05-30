d4cb43982e9463dec7afe3aaf982f4396bb61ce8
Jonathan Adalin <jonathanadalin@gmail.com>
2023-05-30 16:35:45 -0400

    Improve to O(n)

--- 2023-cpp/0001--two-sum/solution.cpp
+++ 2023-cpp/0001--two-sum/solution.cpp
@@ -1,7 +1,24 @@
 class Solution {
-public:
-    // O(n^2)
+  public:
+    // O(n)
     vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> complement_indexes;
+        // O(n) - single pass
+        for (int i = 0; i< nums.size(); i++) {
+            // We can't use the same element twice so we only add to the map at
+            // the end.
+            // O(1) - unordered map search
+            if (complement_indexes.find(nums[i]) != complement_indexes.end()) {
+                return {i, complement_indexes[nums[i]]};
+            }
+            // O(1) - unordered map insert
+            complement_indexes[target - nums[i]] = i;
+        }
+        return {}; // No match found
+    }
+
+    // O(n^2)
+    vector<int> twoSumSlow(vector<int>& nums, int target) {
         // O(n)
         for (int i = 0; i < nums.size() - 1; i++) {
             // O(n)
