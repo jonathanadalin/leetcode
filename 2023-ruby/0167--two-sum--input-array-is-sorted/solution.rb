9bdefef258b374f8643292d37608e37f29aed188
Jonathan Adalin <jonathanadalin@gmail.com>
2023-06-13 00:32:41 -0400

    First try

---
+++ 2023-ruby/0167--two-sum--input-array-is-sorted/solution.rb
@@ -0,0 +1,24 @@
+# @param {Integer[]} numbers
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(numbers, target)
+    i = 0
+    j = 1
+    while i < numbers.length - 1
+        if j == numbers.length
+            i += 1
+            j = i + 1
+        elsif numbers[i] + numbers[j] == target
+            return [i + 1, j + 1]
+        elsif numbers[i] == numbers[j]
+            i += 1
+            j += 1
+        elsif numbers[i] + numbers[j] > target
+            i += 1
+            j = i + 1
+        else j < numbers.length - 1
+            j += 1
+        end
+    end
+end
+
