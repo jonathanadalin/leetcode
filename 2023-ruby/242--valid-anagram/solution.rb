1194a9a5c746b4c8904605a7db207c4e0b9cfb3b
Jonathan Adalin <jonathanadalin@gmail.com>
2023-06-13 11:31:32 -0400

    Add alternate solution

--- 2023-ruby/242--valid-anagram/solution.rb
+++ 2023-ruby/242--valid-anagram/solution.rb
@@ -4,6 +4,23 @@
 
 # Category: Arrays & Hashing
 # Runtime: O(2n)
+def is_anagram_no_hash(s, t)
+    return false if s.length != t.length
+
+    letters = Array.new(26, 0)
+    for letter in s.chars
+        letters[letter.ord - 'a'.ord] += 1
+    end
+    for letter in t.chars
+        letters[letter.ord - 'a'.ord] -= 1
+    end
+    for letter in letters
+        return false if letter != 0
+    end
+    return true 
+end
+
+# Runtime: O(2n)
 def is_anagram(s, t)
     return false if s.length != t.length
 
