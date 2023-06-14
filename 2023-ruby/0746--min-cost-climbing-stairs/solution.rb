54b52efadc83e2c3fc82ec60ac71291f798829e7
Jonathan Adalin <jonathanadalin@gmail.com>
2023-06-14 15:11:15 -0400

    Improve to O(n)

--- 2023-ruby/0746--min-cost-climbing-stairs/solution.rb
+++ 2023-ruby/0746--min-cost-climbing-stairs/solution.rb
@@ -1,7 +1,22 @@
 # @param {Integer[]} cost
 # @return {Integer}
+
+# Category: Dynamic programming
+# Runtime: O(n)
 def min_cost_climbing_stairs(cost)
-    return 0 if cost.length <= 1
+    dp = Array.new(cost.length)
+    for i in 0...cost.length
+        if i < 2
+            dp[i] = cost[i] 
+        else
+            dp[i] = cost[i] + [dp[i - 1], dp[i - 2]].min
+        end
+    end
+    return [dp[cost.length - 1], dp[cost.length - 2]].min
+end
+
+def min_cost_climbing_stairs_recursive(cost)
+    return 0 if cost.length < 2
     return [cost[0], cost[1]].min if cost.length == 2
 
     one_step_cost = cost[0]
