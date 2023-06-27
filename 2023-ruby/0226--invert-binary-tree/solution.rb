dfdb4fe00d7d8d94671f9bae00a86caf4e32ea50
Jonathan Adalin <jonathanadalin@gmail.com>
2023-06-27 16:46:44 -0400

    Add non-recursive solution

--- 2023-ruby/0226--invert-binary-tree/solution.rb
+++ 2023-ruby/0226--invert-binary-tree/solution.rb
@@ -26,3 +26,31 @@ def invert(node)
     invert(node.right)
 end
 
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {TreeNode}
+
+# Runtime: O(n)
+def invert_tree_with_stack(root)
+    stack = []
+    stack.push(root) if root
+    while !stack.empty?
+        node = stack.pop
+        left = node.left
+        node.left = node.right
+        node.right = left
+
+        stack.push(node.left) if node.left
+        stack.push(node.right) if node.right
+    end
+    return root
+end
+
