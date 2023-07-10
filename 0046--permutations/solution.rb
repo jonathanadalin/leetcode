68698cf3630bf43eb8c618d87e7f0293a7cf660f
Jonathan Adalin <jonathanadalin@gmail.com>
2023-07-10 19:13:33 -0400

    Update with runtime analysis

--- 0046--permutations/solution.rb
+++ 0046--permutations/solution.rb
@@ -1,21 +1,24 @@
 # @param {Integer[]} nums
 # @return {Integer[][]}
+
+# Category: Backtracking
+# Runtime: O(N!)
 def permute(nums)
     res = []
-    generate_permutations(res, [], nums)
+    generate_permutations(nums, res, [])
     return res
 end
 
-def generate_permutations(res, curr, nums)
+def generate_permutations(nums, res, curr)
     if curr.length == nums.length
         res.push(curr.clone)
-        return
-    end
-    for i in 0...nums.length
-        if !curr.include?(nums[i])
-            curr.push(nums[i]) 
-            generate_permutations(res, curr, nums)
-            curr.pop
+    else
+        for i in 0...nums.length
+            if !curr.include?(nums[i])
+                curr.push(nums[i])
+                generate_permutations(nums, res, curr)
+                curr.pop
+            end
         end
     end
 end
