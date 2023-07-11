9c0e34410ec96aac8b7b92735f2ea6acfdd30f73
Jonathan Adalin <jonathanadalin@gmail.com>
2023-07-11 00:24:53 -0400

    Improve with memoization

--- 0213--house-robber-ii/solution.rb
+++ 0213--house-robber-ii/solution.rb
@@ -1,15 +1,27 @@
-# @param {Integer[]} nums
-# @return {Integer}
+# Category: Dynamic Programming
+# Runtime: O(2N)
+
+$db = {}
+
 def rob(nums)
     return 0 if nums.length == 0
     return nums[0] if nums.length == 1
-    return [rob_recursive(nums, 1, 0), rob_recursive(nums, 0, 1)].max
+    $db = {}
+    pass_first = rob_memoize(nums, 0, 1)
+    $db = {}
+    take_first = rob_memoize(nums, 1, 0)
+    return [pass_first, take_first].max
 end
 
-def rob_recursive(nums, i, tail)
+def rob_memoize(nums, i, tail)
     return 0 if i >= nums.length - tail
-        
-    return [nums[i] + rob_recursive(nums, i + 2, tail),
-            rob_recursive(nums, i + 1, tail)].max
+    unless $db.has_key?(i)
+        $db[i] = 
+            [
+                nums[i] + rob_memoize(nums, i + 2, tail), # rob
+                rob_memoize(nums, i + 1, tail)            # pass
+            ].max
+    end
+    return $db[i]
 end
 
