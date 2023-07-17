716c013d3e609a7b66d70f073d82990df51a5d32
Jonathan Adalin <jonathanadalin@gmail.com>
2023-07-17 16:22:35 -0400

    Add solution

---
+++ 2023-python3/0238--product-of-array-except-self/solution.py
@@ -0,0 +1,37 @@
+"""
+Category: Arrays & Hashing
+Idea:     The product of all elements except nums[i] is the same as multiplying
+          the product of the elements preceding i and the product of elements 
+          after i. We can calculate each in O(N).
+          TODO: To save on memory, we can store those products in the resulting 
+          array.
+Runtime:  O(N) where N is the size of nums
+"""
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prefixes = [1] * len(nums)
+        for i in range(len(nums)):
+            if i == 0:
+                prefixes[i] = nums[i]
+            else:
+                prefixes[i] = prefixes[i - 1] * nums[i]
+            
+        postfixes = [1] * len(nums)
+        for i in range(len(nums) - 1, -1, -1):
+            if i == len(nums) - 1:
+                postfixes[i] = nums[i]
+            else:
+                postfixes[i] = postfixes[i + 1] * nums[i]
+        
+        result = [0] * len(nums)
+        for i in range(len(nums)):
+            if i == 0:
+                result[i] = postfixes[i + 1]
+            elif i == len(nums) -1:
+                result[i] = prefixes[i - 1]
+            else:
+                result[i] = prefixes[i - 1] * postfixes[i + 1]
+        
+        return result
+                
