a740690ce1e45dd51fd887bf4e2d7b398b4af102
Jonathan Adalin <jonathanadalin@gmail.com>
2023-07-24 18:15:19 -0400

    Improve space complexity

--- 2023-python3/0091--decode-ways/solution.py
+++ 2023-python3/0091--decode-ways/solution.py
@@ -5,11 +5,27 @@ Idea: The number of ways we can decode a "123" is:
       That's n = (n + 1) + (n + 2) with certain conditions.
       We can design a dynamic programming solution with that in mind.
 Time: O(n)
-Space: O(n) - But we can improve to O(1) if we use variables instead of a map for "dp"
+Space: O(1)
 """
 
 class Solution:
+    # Use variables to reduce space complexity.
     def numDecodings(self, s: str) -> int:
+        next_one = 1
+        one_before = next_one
+        two_before = 0
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == "0":
+                next_one = 0
+            else:
+                next_one = one_before
+                if i + 1 < len(s) and int(s[i:i + 2]) in range(0, 27):
+                    next_one += two_before
+            two_before = one_before
+            one_before = next_one
+        return next_one
+
+    def numDecodingsWithMap(self, s: str) -> int:
         dp = { len(s) : 1 }
         for i in range(len(s) - 1, -1, -1):
             # Single digit case
